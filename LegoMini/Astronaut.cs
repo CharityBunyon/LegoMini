9ff68efb2fc00b0c46799c69d44bc49c063b654f
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-03-09 21:28:44 -0500

    learned about composition

--- LegoMini/Astronaut.cs
+++ LegoMini/Astronaut.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LegoMini.Composition.Heads;
+using LegoMini.Composition.Legs;
+using LegoMini.Composition.Torso;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -22,15 +25,22 @@ namespace LegoMini
         //Public property with a private setter
         public string Job { get; private set; }
         public int OxygenLevel { get; set; }
-
-        //Read only property
-        //public string Job {get;}
-
         //made a constructor(assesbility mod, name, params)- they need to have the same name as the class
-        public Astronaut(string name, string job)
+
+
+
+
+        public AstronautTorso Torso { get; set; }
+        public AstronautLegs Legs { get; set; }
+        public AstronautHead Head { get; set; }
+        public Astronaut(string name, string job, AstronautHead head, AstronautTorso torso, AstronautLegs legs)
         {
             Name = name;
             Job = job;
+
+            Head = head;
+            Torso = torso;
+            Legs = legs;
         }
         //First way to set the setter to private or make another method... has to be done inside of the class
         //public void Promote()
@@ -42,6 +52,10 @@ namespace LegoMini
         public void DoYourJob()
         {
             Console.WriteLine($"Doing all my {Job} duties...");
+            Legs.Walk(15);
+            Head.EatPie("Cherry");
+            Torso.Flex();
+            Legs.Walk(10);
         }
     }
 }
---
+++ LegoMini/Composition/Heads/AstronautHead.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMini.Composition.Heads
+{
+    class AstronautHead
+    {
+        public bool Helmeted { get; set; }
+        public string FacialExpression { get; set; }
+        public LegoColor Color { get; set; }
+        public LegoColor EyeColor { get; set; }
+
+
+        public void Talk()
+        {
+            Console.WriteLine($"The {FacialExpression} astronaut says, 'Tell my wife I love her very much.'");
+        }
+
+        public void EatPie(string typeOfPie)
+        {
+            if(typeOfPie == "apple")
+            {
+                Console.WriteLine("MMMM, Pie!");
+            }
+            else
+            {
+                Console.WriteLine("This is not pie!");
+            }
+
+        }
+    }
+    enum LegoColor
+    {
+        Yellow,
+        Red,
+        Green,
+        White,
+        Grey,
+        Blue,
+        Black, 
+        Orange
+    }
+}
